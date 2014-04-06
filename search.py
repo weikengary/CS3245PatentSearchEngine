5358a597e1cd5339b41cdcfc2ff628b32a112df6
David Heryanto <david.heryanto@hotmail.com>
2014-04-07 02:43:21 +0800

    update search, use tf-idf scoring. Result is still bad

--- search.py
+++ search.py
@@ -1,7 +1,10 @@
 import cPickle
 import pickle
 import sys
-from math      import log10
+from math import log10
+from math import sqrt
+from math import pow
+from operator  import itemgetter
 from argparse  import ArgumentParser
 from DictEntry import dict_entry
 from collections     import defaultdict
@@ -18,13 +21,32 @@ TASKS
 6. Results
 '''
 
-
 def main():
-    get_args()
-    query  = get_query()
-    result = search(query)
-    print result
+    global args
+    args    = get_args()
+    query   = get_query()
+    scores  = search(query)
 
+    for score_tuple in scores:
+        print '{: <19}({})'.format(score_tuple[0], score_tuple[1])
+    print '{} matches found.'.format(len(scores))
+
+    # To print without scores
+    # -----------------------
+    # for score_tuple in scores:
+    #     print score_tuple[0]
+
+
+# Get the weight of each field/zone. Normally we would want the sum
+# of the weights of all field/zone to be equal to 1.
+def get_zone_weights():
+    weight = defaultdict()
+    weight['title']       = 0.40
+    weight['description'] = 0.40
+    weight['category']    = 0.20
+    return weight;
+
+# Get and validate command line arguments
 def get_args():
     parser = ArgumentParser()
     parser.add_argument('-d', help='dictionary file')
@@ -32,21 +54,12 @@ def get_args():
     parser.add_argument('-q', help='query file')
     parser.add_argument('-o', help='output of file of results')
 
-    global args
     args = parser.parse_args()
-
     if len(sys.argv) != 9:
         parser.print_help()
         sys.exit(1)
 
-# Get the weight of each field/zone. Normally we would want the sum
-# of the weights of all field/zone to be equal to 1.
-def get_weight():
-    weight = defaultdict()
-    weight['title']       = 0.5
-    weight['description'] = 0.25
-    weight['category']    = 0.25
-    return weight;
+    return args
 
 # Get the query title and description from the xml query file
 def get_query():
@@ -55,21 +68,49 @@ def get_query():
     query['title'], query['description'] = preprocessor.XMLQueryParser(args.q)
     return query;
 
+# Search for matching doc_ids using dictionary and postings and return scores for each document
+# that match at least one query term
 def search(query):
-    query_term_weights = process_query(query)
+    scores = defaultdict(float)
+    query_term_weights, query_normalization = process_query(query)
+    doc_weights_squared = defaultdict(float)
 
     for query_term, query_term_weight in query_term_weights.iteritems():
         postings = get_postings(query_term)
 
+        # Calculate score for each pair (query_term, doc_id)
+        # and update the scores[doc_id] accumulator
+        for doc_id, zone_frequencies in postings.iteritems():
+            doc_weight      = get_doc_weight(query_term, zone_frequencies)
+            scores[doc_id] += query_term_weight * doc_weight
+            doc_weights_squared[doc_id] += pow(doc_weight, 2)
+
+    doc_normalization = {doc_id: sqrt(sum) for doc_id, sum in doc_weights_squared.iteritems()}
+    normalized_scores = normalize_score(scores, query_normalization, doc_normalization)
+    sorted_scores     = sorted(normalized_scores.iteritems(), key = itemgetter(1), reverse=True)
+
+    return sorted_scores;
+
 # Process the query title and description and return the weight for each query term
 def process_query(query):
     preprocessor = PreprocessUtils()
+    # Concatenate the title and description in query. We may want to treat it differently though.
     query_terms  = preprocessor.LinguisticParser(query['title'] + ' ' + query['description'])
-    query_term_freqs   = get_query_term_freqs(query_terms)
-    query_term_weights = get_query_term_weights(query_term_freqs)
-    return query_term_weights
+    query_term_freqs    = get_query_term_freqs(query_terms)
+    query_term_weights  = get_query_term_weights(query_term_freqs)
+    query_normalization = get_normalization_factor(query_term_weights)
+    return query_term_weights, query_normalization
+
+def get_doc_weight(query_term, zone_frequencies):
+    weights    = get_zone_weights()
+    doc_weight = get_tf_idf(query_term, zone_frequencies.get('title', 0))       * weights['title'] + \
+                 get_tf_idf(query_term, zone_frequencies.get('description', 0)) * weights['description'] + \
+                 get_tf_idf(query_term, zone_frequencies.get('category', 0))    * weights['category']
+    return doc_weight
+
 
 # Get a mapping of query_term -> frequency_in_the_query
+# To be used for tf normalization
 def get_query_term_freqs(query_terms):
     query_term_freq = defaultdict(int)
     for query_term in query_terms:
@@ -78,19 +119,40 @@ def get_query_term_freqs(query_terms):
 
 # Get a mapping of query_term -> weight (uses tf-idf)
 def get_query_term_weights(query_term_freqs):
-    query_term_weights    = defaultdict()
-    dictionary, doc_count = get_dictionary()
-
+    query_term_weights = dict()
     for query_term, query_term_freq in query_term_freqs.iteritems():
-        if query_term not in dictionary:
-            query_term_weights[query_term] = 0
-        else:
-            tf  = 1 + log10(query_term_freq)
-            idf = log10(float(doc_count) / dictionary[query_term].doc_freq)
-            query_term_weights[query_term] = tf * idf
-
+        query_term_weights[query_term] = get_tf_idf(query_term, query_term_freq)
     return query_term_weights
 
+# Get (tf * idf) value for the term
+def get_tf_idf(term, term_freq):
+    dictionary, doc_count = get_dictionary()
+
+    if term not in dictionary or term_freq < 1:
+        return 0
+
+    tf  = 1 + log10(term_freq)
+    idf = log10(float(doc_count) / dictionary[term].doc_freq)
+    return tf * idf
+
+# Get the cosine normalization factor
+def get_normalization_factor(weights):
+    sum_of_squares = 0
+    for weight in weights.itervalues():
+        sum_of_squares += pow(weight, 2)
+    return sqrt(sum_of_squares)
+
+def normalize_score(scores, query_normalization, doc_normalization):
+    normalized_scores = defaultdict(float)
+    for doc_id, score in scores.iteritems():
+        if query_normalization > 0 and doc_normalization[doc_id] > 0:
+            normalized_scores[doc_id] = score / doc_normalization[doc_id] / query_normalization
+        else:
+            normalized_scores[doc_id] = score
+    return normalized_scores
+
+
+# Get the dictionary and doc_count from dictionary.txt
 def get_dictionary():
     if 'dictionary' not in globals() or 'doc_count' not in globals():
         with open(args.d, 'r') as dictionary_file:
@@ -100,9 +162,16 @@ def get_dictionary():
             doc_count  = cPickle.load(dictionary_file)
     return dictionary, doc_count
 
+def get_postings(query_term):
+    dictionary, doc_count = get_dictionary()
 
+    if query_term not in dictionary:
+        return dict()
 
+    with open(args.p, 'r') as postings_file:
+        seek_location = dictionary[query_term].posting_pointer
+        postings_file.seek(seek_location)
+        return cPickle.load(postings_file)
 
 # Start search.py
 main()
-print 'hello'
