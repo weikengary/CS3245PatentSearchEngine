a90c089b7a1601279d73272b15c9081e39fb5af4
David Heryanto <david.heryanto@hotmail.com>
2014-04-06 23:14:48 +0800

    add query_term_weight

--- README.txt
+++ README.txt
@@ -33,5 +33,7 @@ I suggest that I should be graded as follows:
 
 == References ==
 
-<Please list any websites and/or people you consulted with for this
-assignment and state their role>
+Patent Retrieval - Allan Hanbury
+https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=4&cad=rja&uact=8&ved=0CEMQFjAD&url=http%3A%2F%2Fallan.hanbury.eu%2Flib%2Fexe%2Ffetch.php%3Fmedia%3Dfntir_2013.pdf&ei=bEdBU4CALY2Hrgeb94C4Bw&usg=AFQjCNF2E669G51uaAJQ98ap3WTa-b7o1w&sig2=xI7ewvZVmjg68-nr1ol2RA&bvm=bv.64367178,d.bmk
+
+
--- search.py
+++ search.py
@@ -1,9 +1,11 @@
 import cPickle
 import pickle
 import sys
+from math      import log10
+from argparse  import ArgumentParser
+from DictEntry import dict_entry
+from collections     import defaultdict
 from PreprocessUtils import PreprocessUtils
-from argparse        import ArgumentParser
-from DictEntry       import dict_entry
 
 '''
 TASKS
@@ -16,33 +18,91 @@ TASKS
 6. Results
 '''
 
+
 def main():
-    init()
+    get_args()
+    query  = get_query()
+    result = search(query)
+    print result
 
 def get_args():
     parser = ArgumentParser()
-    parser.add_argument('-d', help = 'dictionary file')
-    parser.add_argument('-p', help = 'postings file')
-    parser.add_argument('-q', help = 'query file')
-    parser.add_argument('-o', help = 'output of file of results')
+    parser.add_argument('-d', help='dictionary file')
+    parser.add_argument('-p', help='postings file')
+    parser.add_argument('-q', help='query file')
+    parser.add_argument('-o', help='output of file of results')
+
+    global args
     args = parser.parse_args()
 
     if len(sys.argv) != 9:
         parser.print_help()
         sys.exit(1)
 
-    return args
+# Get the weight of each field/zone. Normally we would want the sum
+# of the weights of all field/zone to be equal to 1.
+def get_weight():
+    weight = defaultdict()
+    weight['title']       = 0.5
+    weight['description'] = 0.25
+    weight['category']    = 0.25
+    return weight;
 
-def init():
-    args = get_args()
+# Get the query title and description from the xml query file
+def get_query():
+    query = defaultdict()
+    preprocessor = PreprocessUtils()
+    query['title'], query['description'] = preprocessor.XMLQueryParser(args.q)
+    return query;
 
-    dict_file     = open(args.d, 'r')
-    postings_file = open(args.p, 'r')
-    query_file    = open(args.q, 'r')
-    output_file   = open(args.o, 'w')
+def search(query):
+    query_term_weights = process_query(query)
 
-    dictionary = pickle.load(dict_file)
-    print 'hello'
+    for query_term, query_term_weight in query_term_weights.iteritems():
+        postings = get_postings(query_term)
+
+# Process the query title and description and return the weight for each query term
+def process_query(query):
+    preprocessor = PreprocessUtils()
+    query_terms  = preprocessor.LinguisticParser(query['title'] + ' ' + query['description'])
+    query_term_freqs   = get_query_term_freqs(query_terms)
+    query_term_weights = get_query_term_weights(query_term_freqs)
+    return query_term_weights
+
+# Get a mapping of query_term -> frequency_in_the_query
+def get_query_term_freqs(query_terms):
+    query_term_freq = defaultdict(int)
+    for query_term in query_terms:
+        query_term_freq[query_term] += 1
+    return query_term_freq
+
+# Get a mapping of query_term -> weight (uses tf-idf)
+def get_query_term_weights(query_term_freqs):
+    query_term_weights    = defaultdict()
+    dictionary, doc_count = get_dictionary()
+
+    for query_term, query_term_freq in query_term_freqs.iteritems():
+        if query_term not in dictionary:
+            query_term_weights[query_term] = 0
+        else:
+            tf  = 1 + log10(query_term_freq)
+            idf = log10(float(doc_count) / dictionary[query_term].doc_freq)
+            query_term_weights[query_term] = tf * idf
+
+    return query_term_weights
+
+def get_dictionary():
+    if 'dictionary' not in globals() or 'doc_count' not in globals():
+        with open(args.d, 'r') as dictionary_file:
+            # dictionary and doc_count are going to be reused again in search, so we set it to global
+            global dictionary, doc_count
+            dictionary = cPickle.load(dictionary_file)
+            doc_count  = cPickle.load(dictionary_file)
+    return dictionary, doc_count
+
+
+
 
 # Start search.py
 main()
+print 'hello'
