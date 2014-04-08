f3e6ce01587014a7656255222d52a5a3b374bd73
David Heryanto <david.heryanto@hotmail.com>
2014-04-08 11:11:30 +0800

    Update feedback

--- feedback.py
+++ feedback.py
@@ -15,32 +15,43 @@ def get_args():
         parser.print_help()
         sys.exit(1)
 
-def write_result(file_path):
+def write_result(file_path, index):
     preprocessor = PreprocessUtils()
     tagValues, words = preprocessor.XMLPatentDocParser(file_path)
     codeValues = preprocessor.IPCCodeCategoryParser()
 
-    with open(file_path, 'a') as output_file:
+    with open(args.o, 'a') as output_file:
+        output_file.write(str(index) + '. ')
         for tagName, content in tagValues.iteritems():
+
             tagName = tagName.strip()
             content = content.strip()
 
             if tagName == 'Patent Number':
                 output_file.write(content)
-                output_file.write(os.linesep)
+                output_file.write('\n')
             elif tagName == 'Title':
                 output_file.write('Title    :' + content)
-                output_file.write(os.linesep)
+                output_file.write('\n')
             elif tagName == 'Abstract':
                 output_file.write('Abstract :' + content)
-                output_file.write(os.linesep)
+                output_file.write('\n')
             elif tagName == 'IPC Subclass':
-                output_file.write('Subclass :' + content)
-                output_file.write(os.linesep)
+                output_file.write('Subclass :' + content + ' - ' + codeValues.get(content, ''))
+                output_file.write('\n')
+
+        output_file.write('\n')
+
+
+
 
 get_args()
-query_file_path = os.path.join('query', args.r)
-with open(query_file_path) as result_file:
+if os.path.isfile(args.o):
+    os.remove(args.o)
+
+with open(args.r) as result_file:
+    i = 1
     for doc_id in result_file:
         doc_id_path = os.path.join('patsnap-corpus', doc_id.strip() + '.xml')
-        write_result(doc_id_path)
+        write_result(doc_id_path, i)
+        i += 1
